c99176b594bc09e651a9b46ee208fccaafb03880
Arunkumar1712 <arunk05691@gail.com>
2024-04-16 21:03:25 +0530

    update

--- Assignment/Day04/day04.c
+++ Assignment/Day04/day04.c
@@ -1,47 +1,38 @@
 #include <stdio.h>
 
-int main(){
-    int SI_No,Id;
-    char name[10],gender,phoneNo[10],address[50];
+int main() {
+    int SI_No, Id;
+    char name[20], gender, phoneNo[20], address[100];
     float salary;
 
     printf("Enter the Employee Detail\n");
     printf("Enter SI_No:\n");
-    scanf("%d",&SI_No);
-
-  
+    scanf("%d", &SI_No);
 
     printf("Enter Id:\n");
-    scanf("%d",&Id);
+    scanf("%d", &Id);
 
     printf("Enter name:\n");
-    scanf("%9s",&name);
+    scanf("%19s", name);
 
     printf("Enter gender:\n");
-    scanf(" %c",&gender);
+    scanf(" %c", &gender);
 
     printf("Enter phoneNo:\n");
-    scanf("%s",&phoneNo);
+    scanf("%19s", phoneNo);
 
     printf("Enter address:\n");
-    scanf("%s",&address);
+    scanf("%99s", address);
 
     printf("Enter salary:\n");
-    scanf("%f",&salary);
+    scanf("%f", &salary);
 
     printf("\n========================================\n");
     printf("            Employee Details            \n");
     printf("========================================\n");
     printf("SLNo\tID\tName\tGender\tPhno\tAddress\tSalary\n");
     printf("----\t---\t----\t----\t-------\t------\t-------\n");
-    printf("%d\t%d\t%s\t%c\t%s\t%s\t%.1f\n", SI_No, Id, name, gender, phoneNo, address , salary);
+    printf("%d\t%d\t%s\t%c\t%s\t%s\t%.1f\n", SI_No, Id, name, gender, phoneNo, address, salary);
 
-    // Print table with user input
-    // int userInput;
-    // printf("\nTable: ");
-    // scanf("%d", &userInput);
-    // printf("Table: %d\n", userInput);
+    return 0;
 }
-
-
-
