80d61723dd4e878977a3ded1777d1aa4bce6305a
Arunkumar1712 <arunk05691@gail.com>
2024-04-16 23:11:01 +0530

    update

--- Assignment/Day05/squarenopattern.c
+++ Assignment/Day05/squarenopattern.c
@@ -1,3 +1,4 @@
+/*Program to print numbers in square pattern*/
 #include <stdio.h>
 
 int main()
@@ -5,13 +6,19 @@ int main()
     int number;
     printf("Enter the number :");
     scanf("%d", &number);
-    for (int i = 1; i <= number*number; i++)
+    for (int i = 1; i <= number * number; i++)
     {
-        if(i%number == 0)
-        printf("%d\n",i);
+        if (i % number == 0)
+            printf("%d\n", i);
         else
-        printf("%d*",i);
-        
-        
+            printf("%d*", i);
     }
 }
+/*Output we got is
+Enter the number :5
+1*2*3*4*5
+6*7*8*9*10
+11*12*13*14*15
+16*17*18*19*20
+21*22*23*24*25
+*/
