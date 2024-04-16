40821ee3d8f123f5193219ffd3285886b488b5f5
Arunkumar1712 <116170537+Arunkumar1712@users.noreply.github.com>
2024-04-16 21:50:52 +0530

    update

---
+++ Assignment/Day05/righttriangle.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int number;
+    printf("Enter the number :");
+    scanf("%d", &number);
+    for (int i = 1; i < number; i++)
+    {
+        for(int j=0;j<i;j++){
+        printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = number; i >0; i--)
+    {
+        for(int j=0;j<i;j++){
+        printf("*");
+        }
+        printf("\n");
+    }
+    
+}
